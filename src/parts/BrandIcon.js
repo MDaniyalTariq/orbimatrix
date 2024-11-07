6fef54d9abf68dc6b2705485c3e002b68cadfcf3
MDaniyalTariq <153484599+MDaniyalTariq@users.noreply.github.com>
2024-11-07 19:08:21 +0500

    "Updated BrandIcon component to use 'primary-white' instead of 'light-white' and renamed 'light-white' to 'primary-white' in tailwind.config.js"

--- src/parts/BrandIcon.js
+++ src/parts/BrandIcon.js
@@ -7,7 +7,7 @@ export default function BrandIcon() {
   return (
     <Button className="flex items-center" type="link" href="/">
       <img src={logoPath} alt="Brand Logo" className="h-16 mr-2 w-4xl" />
-      <p className="text-4xl font-medium text-theme-blue dark:text-light-white">
+      <p className="text-4xl font-medium text-theme-blue dark:text-primary-white">
         Orbi
         <span className="text-theme-purple ">matrix</span>
       </p>
